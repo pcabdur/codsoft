eb263bc1276dcc8ad9a4fa4631aa972d88978b52
pcabdur <pcabdur3@gmail.com>
2024-09-26 14:45:01 +0530

    Add files via upload

---
+++ javacodsoft1.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int numberToGuess = random.nextInt(100) + 1;
+        int numberOfTries = 0;
+        boolean hasGuessedCorrectly = false;
+
+        System.out.println("Welcome to the Number Game!");
+        System.out.println("Guess the number between 1 and 100");
+
+        while (!hasGuessedCorrectly) {
+            System.out.print("Enter your guess: ");
+            int guess = scanner.nextInt();
+            numberOfTries++;
+
+            if (guess < numberToGuess) {
+                System.out.println("Too low! Try again.");
+            } else if (guess > numberToGuess) {
+                System.out.println("Too high! Try again.");
+            } else {
+                hasGuessedCorrectly = true;
+                System.out.println("Congratulations! You've guessed the correct number.");
+                System.out.println("It took you " + numberOfTries + " tries.");
+            }
+        }
+        
+        scanner.close();
+    }
+}
